8f38a238a6ef4b864e5deeb65fd6827a6cd7997a
ThyShadoe <olbenarioaxelrose+github@protonmail.com>
2025-09-21 23:27:56 +0800

    started work on script.js

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// ====== UTILITIES ======
+const $ = (s) => document.querySelector(s);
+const $$ = (s) => Array.from(document.querySelectorAll(s));
+const out = $("#output");
+const preview = $("#preview");
+const STORAGE_KEY = "codelab-web";
+
+const escapeHtml = (s) =>
+  String(s).replace(
+    /[&<>"]/g,
+    (c) =>
+      ({
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "%gt;",
+        '"': "&quot;",
+      })[c],
+  );
+
+// LOGS
+function log(msg, type = "info") {
+  const color =
+    type === "error"
+      ? "var(--err)"
+      : type === "warn"
+        ? "warn(--warn)"
+        : "var(--brand)";
+
+  const time = new Date().toLocaleDateString();
+
+  const line = document.createElement("div");
+
+  line.innerHTML = `<span style="color": ${color}>[${time}]</span> ${escapeHtml(msg)}`;
+
+  out.appendChild(line);
+  out.scrollTop = out.scrollHeight;
+}
+
+function clearOut() {
+  out.innerHTML = "";
+}
+
+$("#clearOut")?.addEventListener("click", clearOut);
+
+function makeEditor(id, mode) {
+  const ed = ace.edit(id, {
+    theme: "ace/theme/nord",
+    mode,
+    tabSize: 2,
+    useSoftTabs: true,
+    showPrintMargin: false,
+    wrap: true,
+  });
+
+  ed.session.setUseWrapMode(true);
+
+  ed.commands.addCommand({
+    name: "run",
+    bindKey: {
+      win: "Ctrl-Enter",
+      mac: "Command-Enter",
+    },
+    exec() {
+      runWeb(false);
+    },
+  });
+
+  ed.commands.addCommand({
+    name: "save",
+    bindKey: {
+      win: "Ctrl-S",
+      mac: "Command-S",
+    },
+    exec() {
+      saveProject();
+    },
+  });
+
+  return ed;
+}
+
+const ed_html = makeEditor("ed_html", "ace/mode/html");
+const ed_css = makeEditor("ed_css", "ace/mode/css");
+const ed_js = makeEditor("ed_js", "ace/mode/javascript");
+
+const TAB_ORDER = ["html", "css", "js"];
+
+const wraps = Object.fromEntries(
+  $$("#webEditors .editor-wrap").map((w) => [w.dataset.pane, w]),
+);
